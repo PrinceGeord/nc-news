e57a5b3f1e715cd029ec023020626f6c95b0bf72
PrinceGeord <lukekidwell@live.co.uk>
2023-10-23 12:03:56 +0100

    setting up general filestructure

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <h1 className="title">NC-NEWS</h1>
+      <img
+        className="logo"
+        src="https://leeds.tech/wp-content/uploads/2017/04/logo-northcoders-1024x227.png"
+        alt="nc-news logo"
+      ></img>
+    </header>
+  );
+};
+
+export default Header;
