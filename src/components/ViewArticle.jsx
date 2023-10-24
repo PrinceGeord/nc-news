cb2c1eb7b2976660cf7096ea75aedff0f98dfb42
PrinceGeord <lukekidwell@live.co.uk>
2023-10-24 16:32:31 +0100

    Commiting changes before trying something experimental and everything goes wrong

--- src/components/ViewArticle.jsx
+++ src/components/ViewArticle.jsx
@@ -4,12 +4,14 @@ import Loader from "./Loader";
 import Error from "./Error";
 import { useParams } from "react-router-dom";
 import CommentSection from "./CommentSection";
+import VoteCount from "./VoteCount";
 
 const ViewArticle = () => {
   const [articleChoice, setArticleChoice] = useState("");
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const { article_id } = useParams();
+  const [voteChange, setVoteChange] = useState(false);
   useEffect(() => {
     setIsLoading(true);
     api
@@ -43,23 +45,43 @@ const ViewArticle = () => {
     <>
       <h1>{articleChoice.title}</h1>
       <h3>Author: {articleChoice.author}</h3>
-      <h4>
-        Votes: {articleChoice.votes}{" "}
-        <input
-          type="image"
-          alt="click here to upvote this article"
-        ></input>
-        <input
-          type="image"
-          alt="click here to downvote this article"
-        ></input>
-      </h4>
+
       <img
         src={articleChoice.article_img_url}
         alt={`Picture relating to ${articleChoice.topic}`}
       />
       <p>{articleChoice.body}</p>
-      <h5>Comments: {articleChoice.comment_count}</h5>
+      <section className="vote-section">
+        <input
+          className="button-img"
+          type="image"
+          alt="click here to upvote this article"
+          src="https://www.clipartmax.com/png/middle/223-2235237_owl-emoji-png.png"
+          onClick={(event) => {
+            api.upVote(article_id);
+            event.currentTarget.disabled = true;
+            setVoteChange(true);
+          }}
+        />
+        <input
+          className="button-img"
+          type="image"
+          alt="click here to downvote this article"
+          src="https://www.emoji.co.uk/files/twitter-emojis/smileys-people-twitter/10633-athletic-shoe.png"
+          onClick={(event) => {
+            api.downVote(article_id);
+            event.currentTarget.disabled = true;
+            setVoteChange(true);
+          }}
+        />
+      </section>
+      <h3>Votes: {articleChoice.votes}</h3>
+      <VoteCount
+        votes={articleChoice.votes}
+        voteChange={voteChange}
+        setVoteChange={setVoteChange}
+      />
+      <h3>Comments: {articleChoice.comment_count}</h3>
       <CommentSection article_id={articleChoice.article_id} />
     </>
   );
