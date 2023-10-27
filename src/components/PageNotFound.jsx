c3831a10a9346ab32648cb8ec0a9b61d5ac89185
PrinceGeord <lukekidwell@live.co.uk>
2023-10-27 13:49:10 +0100

    set up error for when non-existent path is entered

---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,10 @@
+const PageNotFound = () => {
+  return (
+    <h2>
+      Oops we seem to have gotten a bit lost. Click home so we can
+      find our way back!
+    </h2>
+  );
+};
+
+export default PageNotFound;
